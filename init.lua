b6e2329776a221661fdfc77c5d8718cb35cd9667
TheRedPillOw <janosc.beck@gmail.com>
2025-12-29 14:54:58 +0100

    added Platform IO plugin

--- init.lua
+++ init.lua
@@ -996,6 +996,11 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  {
+    'sbatin/platformio.nvim',
+    dependencies = { 'numToStr/FTerm.nvim' },
+  },
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
